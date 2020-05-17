16692773a5af95d5dcc0864dc2bf670962b10730
Fipeman <fsilvagalleguillos@gmail.com>
2020-05-17 19:19:54 -0400

    identico

--- cachipum/juego.rb
+++ cachipum/juego.rb
@@ -4,32 +4,32 @@ computador= rand(0..2)
 # 0=piedra , 1=papel, 2=tijera
 
 if jugador=='piedra' && computador==2
-    puts "Computador juega tijera"
-    puts "Ganaste"
+    puts "• Computador juega tijera"
+    puts "• Ganaste"
 elsif jugador=='piedra' && computador==1
-    puts "Computador juega papel"
-    puts "Perdiste"
+    puts "• Computador juega papel"
+    puts "• Perdiste"
 elsif jugador=='piedra' && computador==0
-    puts "Computador juega piedra"
-    puts "Empataste"
+    puts "• Computador juega piedra"
+    puts "• Empataste"
 elsif jugador=='papel' && computador==2
-    puts "Computador juega tijera"
-    puts "Perdiste"
+    puts "• Computador juega tijera"
+    puts "• Perdiste"
 elsif jugador=='papel' && computador==1
-    puts "Computador juega papel"
-    puts "Empataste"
+    puts "• Computador juega papel"
+    puts "• Empataste"
 elsif jugador=='papel' && computador==0
-    puts "Computador juega piedra"
-    puts "Ganaste"
+    puts "• Computador juega piedra"
+    puts "• Ganaste"
 elsif jugador=='tijera' && computador==2
-    puts "Computador juega tijera"
-    puts "Empataste"
+    puts "• Computador juega tijera"
+    puts "• Empataste"
 elsif jugador=='tijera' && computador==1
-    puts "Computador juega papel"
-    puts "Ganaste"
+    puts "• Computador juega papel"
+    puts "• Ganaste"
 elsif jugador=='tijera' && computador==0
-    puts "Computador juega piedra"
-    puts "Perdiste"
+    puts "• Computador juega piedra"
+    puts "• Perdiste"
 else
     puts "Argumento inválido: Debe ser piedra, papel o tijera." 
 end
