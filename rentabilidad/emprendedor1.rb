5c59be668895739cb3e247d38ffb296fda78a0a8
Fipeman <fsilvagalleguillos@gmail.com>
2020-05-18 20:44:19 -0400

    emprendedor done

--- rentabilidad/emprendedor1.rb
+++ rentabilidad/emprendedor1.rb
@@ -2,6 +2,6 @@ p = ARGV[0].to_f
 users = ARGV[1].to_i
 g = ARGV[2].to_f
 
-uti = p * users -g
+u = p * users -g
 
-puts uti * 0.65 if uti > 0
+puts u * 0.65 if u > 0
--- rentabilidad/emprendedor2.rb
+++ rentabilidad/emprendedor2.rb
@@ -4,6 +4,6 @@ users_premium= ARGV[2].to_i
 users_free=ARGV[3].to_i
 g = ARGV[4].to_f
 
-uti = ((p * users) + ((p * users_free) * 0) + ((p * users_premium) * 2)) - g
+u = ((p * users) + ((p * users_free) * 0) + ((p * users_premium) * 2)) - g
 
-puts uti * 0.65 if uti > 0
+puts u * 0.65 if u > 0
