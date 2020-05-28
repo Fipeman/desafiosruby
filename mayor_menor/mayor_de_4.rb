b309f9ad1033f56f5eca90ab2c539a7b92a67c32
Fipeman <fsilvagalleguillos@gmail.com>
2020-05-27 20:58:32 -0400

    >4

--- mayor_menor/mayor_de_4.rb
+++ mayor_menor/mayor_de_4.rb
@@ -3,18 +3,28 @@ numero2 = ARGV[1].to_i
 numero3 = ARGV[2].to_i
 numero4 = ARGV[3].to_i
 
-if numero4 == nil 
+if numero4 != nil 
 
-
-if numero1 >= numero2 && numero1 >= numero3 && numero1 >= numero4 
-   puts numero1
-elsif  
-   numero2 >= numero1 && numero2 >= numero3 && numero2 >= numero4
-   puts numero2
-elsif
-   numero3 >= numero1 && numero3 >= numero2 && numero3 >= numero4
-   puts numero3
+   if numero1 >= numero2 && numero1 >= numero3 && numero1 >= numero4 
+      puts numero1
+   elsif  
+      numero2 >= numero1 && numero2 >= numero3 && numero2 >= numero4
+      puts numero2
+   elsif
+      numero3 >= numero1 && numero3 >= numero2 && numero3 >= numero4
+      puts numero3
+   else
+      numero4 >= numero1 && numero4 >= numero2 && numero4 >= numero3
+      puts numero4
+   end
 else
-    numero4 >= numero1 && numero4 >= numero2 && numero4 >= numero3
-    puts numero4
-end     
+   if numero1 >= numero2 && numero1 >= numero3
+      puts "#{numero1}"
+   elsif  
+      numero2 >= numero1 && numero2 >= numero3
+      puts "#{numero2}"
+   else
+      numero3 >= numero1 && numero3 >= numero2
+      puts "#{numero3}"
+   end
+end        
