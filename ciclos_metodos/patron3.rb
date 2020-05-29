72db6a02cfe89e3c9cd72c21535ea286afbca363
Fipeman <fsilvagalleguillos@gmail.com>
2020-05-29 15:32:37 -0400

    mejora

--- ciclos_metodos/patron3.rb
+++ ciclos_metodos/patron3.rb
@@ -1,10 +1,12 @@
 n = ARGV[0].to_i
 
 n.times do |i|
-    if i%4 == 0 || i%4 == 2
-        print"..*"
+    if    i%3 == 0
+        print".."
+    elsif i%3 == 1
+        print"**"
     else
-        print"*||"
-    end
+        print"||"
+    end    
 end
 puts        
