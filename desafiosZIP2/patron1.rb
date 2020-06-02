ddaa88fee3bc2c8c071621cb0c2b6197dcb595db
Fipeman <fsilvagalleguillos@gmail.com>
2020-06-01 21:03:18 -0400

    patrones

---
+++ desafiosZIP2/patron1.rb
@@ -0,0 +1,10 @@
+n = ARGV[0].to_i
+
+n.times do |i|
+    if i.odd?
+    print "*"
+    print "."
+    end
+end
+
+puts
---
+++ desafiosZIP2/patron3.rb
@@ -0,0 +1,10 @@
+n = ARGV[0].to_i
+
+n.times do |i|
+    if i.odd?
+    print "1"
+    print "2"
+    end
+end
+
+puts
