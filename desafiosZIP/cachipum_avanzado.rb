322cba6217708673760014b01d3cf915ed7160aa
Fipeman <fsilvagalleguillos@gmail.com>
2020-06-12 21:43:34 -0400

    casi

--- desafiosZIP/cachipum_avanzado.rb
+++ desafiosZIP/cachipum_avanzado.rb
@@ -1,3 +1,13 @@
+def cachipum (j1,j2)
+    if j1==1 && j2==2 || j1==2 && j2==3 || j1==3 && j2==1
+     puts "Gana Jugador 2"
+    elsif j1==1 && j2==3 || j1==2 && j2==1 || j1==3 && j2==2
+     puts "Gana Jugador 1"
+    else          
+     puts "empate"  
+   end
+ end    
+
 puts "turno Jugador 1"
 puts "----------------"
 puts "Elige una opción"
@@ -8,22 +18,13 @@ puts "3) tijera"
 puts "4) salir"
 puts "----------------"
 
-def cachipum (j1,j2)
-   if j1==1 && j2==2 || j1==2 && j2==3 || j1==3 && j2==1
-    puts "Gana Jugador 2"
-   elsif j1==1 && j2==3 || j1==2 && j2==1 || j1==3 && j2==2
-    puts "Gana Jugador 1"
-   else          
-    puts "empate"  
-  end
-end    
-
 opcion_j1 = gets.chomp.to_i
 while opcion_j1 != 4
-    if opcion_j1 <1 || opcion_j1 >4
+    if opcion_j1 < 1 || opcion_j1 > 4
         puts "Ingresar opción válida"
         opcion_j1 = gets.chomp.to_i
-    else    
+        else
+        puts opcion_j1        
         puts "turno Jugador 2"
         puts "Elige una opción"
         puts "1) piedra"
@@ -32,13 +33,15 @@ while opcion_j1 != 4
         puts "4) salir"
         opcion_j2 = gets.chomp.to_i
         while opcion_j2 != 4
-            if opcion_j2 <1 || opcion_j2 >4
-                puts "Ingresar opción válida"
-                opcion_j2 = gets.chomp.to_i
-            else    
-                cachipum(opcion_j1,opcion_j2)
-                break
-            end
+        if opcion_j2 < 1 || opcion_j2 > 4
+        puts "Ingresar opción válida"
+        opcion_j2 = gets.chomp.to_i
+        else    
+        cachipum(opcion_j1,opcion_j2)
+        end
+        puts "saliendo"  
+        opcion_j2 = 4      
         end        
     end
-end             
+    opcion_j1 = 4
+    end             
