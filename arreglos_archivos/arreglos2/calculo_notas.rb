f7bd09f21e756d049ae106d5f8c7dedfdf77084c
Fipeman <fsilvagalleguillos@gmail.com>
2020-06-12 21:11:56 -0400

    me falta ya estoy enredado

---
+++ arreglos_archivos/arreglos2/calculo_notas.rb
@@ -0,0 +1,7 @@
+def nota_mas_alta(notas)
+    notas.map { |e| e.to_i }
+    return notas.max
+end
+
+puts nota_mas_alta(open('./notas.data')) 
+
--- arreglos_archivos/arreglos2/proyecciones.rb
+++ arreglos_archivos/arreglos2/proyecciones.rb
@@ -1,4 +1,4 @@
-file = File.open('./ventas.json').read
+file = File.open('./ventas_base.db').read
 file = file.split(',')
 
 file.map!{|nu| nu.to_f}
@@ -6,13 +6,13 @@ file.map!{|nu| nu.to_f}
 def projection(sales, percent, first_month, last_month)
  first_month = first_month - 1
  last_month = last_month - 1   
-
- total = sales[start..finish].sum
- projecteichon = total_sales * (1+percent/100.0)
+ total = sales[first_month..last_month].sum
+ projecteichon = total * (1+percent/100.0)
 end
 
-data.push(file, 10, 1, 6)
-data.push(file, 10, 1, 6)
+data1 = ("%.2f" % projection(file, 10, 1, 6))
+data2 = ("%.2f" % projection(file, 20, 7, 12))
 
+data = data1+"\n"+data2 
 
-file.write('./resultados.data', data)
+File.write('./resultados.data', data)
